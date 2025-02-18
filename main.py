486dec6d89a391600451191040f7ae77cdc4a014
VanoKi <kirsvano@gmail.com>
2025-02-18 21:16:02 +0300

    faet: count size of the directory and amount of items

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from pathlib import Path
 from icecream import ic
 import humanize
 
-# dir = r'C:\Users\Vanoha\Downloads'
 directory = Path('C:/Users/Vanoha/downloads')
-# print(dir)
-# for i in directory.iterdir():
-#     ic(i)
-# ic(directory.stat().st_size)
-ic(humanize.naturalsize(directory.stat().st_size, binary=True))
-# [ic(i) for i in directory.stat()]
-# help(os.stat_result)
+total_size = 0
+items = 0
+for i in directory.iterdir():
+    total_size += i.stat().st_size
+    items += 1
+
+ic(items)
+ic(total_size)
+ic(humanize.naturalsize(total_size))
+

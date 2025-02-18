f971422dcb79d6f54bc9734f06d6f52e445342f2
VanoKi <kirsvano@gmail.com>
2025-02-18 21:06:03 +0300

    feat: add humanize

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from pathlib import Path
 from icecream import ic
+import humanize
 
 # dir = r'C:\Users\Vanoha\Downloads'
 directory = Path('C:/Users/Vanoha/downloads')
 # print(dir)
 # for i in directory.iterdir():
 #     ic(i)
-ic(directory.stat().st_size)
+# ic(directory.stat().st_size)
+ic(humanize.naturalsize(directory.stat().st_size, binary=True))
 # [ic(i) for i in directory.stat()]
 # help(os.stat_result)

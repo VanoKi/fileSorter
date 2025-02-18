9602c749d6584fce3fbb1ce0f27c09b4c89f8a5b
VanoKi <kirsvano@gmail.com>
2025-02-18 16:51:22 +0300

    feat: get size of the directory

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import pathlib
+import os
+from pathlib import Path
+from icecream import ic
 
 # dir = r'C:\Users\Vanoha\Downloads'
-dir = pathlib.Path('C:/Users/Vanoha/downloads')
-
-print(dir)
+directory = Path('C:/Users/Vanoha/downloads')
+# print(dir)
+# for i in directory.iterdir():
+#     ic(i)
+ic(directory.stat().st_size)
+# [ic(i) for i in directory.stat()]
+# help(os.stat_result)

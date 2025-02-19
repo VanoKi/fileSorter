a7133e305ebf4041fd0073e2bebc531ac17a8138
VanoKi <kirsvano@gmail.com>
2025-02-19 10:35:20 +0300

    feat: add sort by extension

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 from icecream import ic
 import humanize
+import time
 
+start = time.time()
 directory = Path('C:/Users/Vanoha/downloads')
 def directory_size(folder):
     total_size = 0
@@ -13,5 +15,17 @@ def directory_size(folder):
             # ic(i.stat().st_size)
             total_size += i.stat().st_size
     return total_size
+# ic(humanize.naturalsize(directory_size(directory)))
+dict = {}
+for i in directory.iterdir():
+    if i.is_file():
+        # ic(i.suffix)
+        key = i.suffix
+        if key in dict:
+            dict[key] += 1
+        else:
+            dict[key] = 1
 
-ic(humanize.naturalsize(directory_size(directory)))
+end = time.time()
+ic(end - start)
+ic(dict)
